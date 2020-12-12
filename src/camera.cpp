3ca176f31d0d1f54a256f7fef6476c2d00a10456
aliefhooghe <arthurliefh@orange.fr>
2020-12-13 00:17:40 +0100

    Camera : complete ray sampling

--- src/camera.cpp
+++ src/camera.cpp
@@ -6,43 +6,56 @@ namespace Xrender {
 
 
     camera camera::from_sensor_lens_distance(
-        unsigned int sensor_pixel_width, unsigned int sensor_pixel_height,
-        float sensor_real_width, float sensor_real_height,
-        float diaphragm_radius, float focal_length, float sensor_lens_distance)
+        unsigned int image_pixel_width,
+        unsigned int image_pixel_height,
+        float sensor_real_width,
+        float sensor_real_height,
+        float diaphragm_radius,
+        float focal_length,
+        float sensor_lens_distance)
     {
         return camera{
-            sensor_pixel_width, sensor_pixel_height,
+            image_pixel_width, image_pixel_height,
             sensor_real_width, sensor_real_height,
             diaphragm_radius, focal_length, sensor_lens_distance
         };
     }
 
     camera camera::from_focus_distance(
-        unsigned int sensor_pixel_width, unsigned int sensor_pixel_height,
-        float sensor_real_width, float sensor_real_height,
-        float diaphragm_radius, float focal_length, float focus_distance)
+        unsigned int image_pixel_width,
+        unsigned int image_pixel_height,
+        float sensor_real_width,
+        float sensor_real_height,
+        float diaphragm_radius,
+        float focal_length,
+        float focus_distance)
     {
         const auto sensor_lens_distance = (focal_length * focus_distance) / (focus_distance - focal_length);
         return camera{
-            sensor_pixel_width, sensor_pixel_height,
+            image_pixel_width, image_pixel_height,
             sensor_real_width, sensor_real_height,
             diaphragm_radius, focal_length, sensor_lens_distance
         };
     }
 
     camera::camera(
-            unsigned int sensor_pixel_width, unsigned int sensor_pixel_height,
-            float sensor_real_width,float sensor_real_height,
-            float diaphragm_radius, float focal_length, float sensor_lens_distance)
-    :   _sensor_pixel_half_width{sensor_pixel_width / 2u}, _sensor_pixel_half_height{sensor_pixel_height / 2u},
-        _pixel_width{sensor_real_width / static_cast<float>(sensor_pixel_width)}, 
-        _pixel_height{sensor_real_height / static_cast<float>(sensor_pixel_height)}, 
+            unsigned int image_pixel_width,
+            unsigned int image_pixel_height,
+            float sensor_real_width,
+            float sensor_real_height,
+            float diaphragm_radius,
+            float focal_length,
+            float sensor_lens_distance)
+    :   _image_pixel_half_width{image_pixel_width / 2u},
+        _image_pixel_half_height{image_pixel_height / 2u},
+        _pixel_width{sensor_real_width / static_cast<float>(image_pixel_width)}, 
+        _pixel_height{sensor_real_height / static_cast<float>(image_pixel_height)}, 
         _diaphragm_radius{diaphragm_radius}, _focal_length{focal_length},
         _sensor_lens_distance{sensor_lens_distance}
     {
     }
 
-    vecf camera::_sample_lens_point()
+    vecf camera::_sample_lens_point() const noexcept
     {
         
         vecf len_pos = {0.f, 0.f, 0.f};
@@ -50,10 +63,10 @@ namespace Xrender {
         return _diaphragm_radius * len_pos;
     }
 
-    void camera::sample_ray(unsigned int px, unsigned int py, vecf& pos, vecf& dir)
+    void camera::sample_ray(unsigned int px, unsigned int py, vecf& pos, vecf& dir) const noexcept
     {
-        const int signed_px = static_cast<int>(px) - static_cast<int>(_sensor_pixel_half_width);
-        const int signed_py = static_cast<int>(py) - static_cast<int>(_sensor_pixel_half_height);
+        const float signed_px = (static_cast<int>(px) - static_cast<int>(_image_pixel_half_width)) + rand::uniform();
+        const float signed_py = (static_cast<int>(py) - static_cast<int>(_image_pixel_half_height)) + rand::uniform();
 
         // Compute the pixel origin position on the sensor
         const vecf pixel_origin = 
@@ -67,11 +80,9 @@ namespace Xrender {
         pos = _sample_lens_point();
 
         // // Compute the outgoing direction (from camera to scenes)
-        // dir = (pos * (_focal_length / _sensor_lens_distance - 1.f) - 
-        //         (_focal_length / _sensor_lens_distance) * pixel_origin
-        //     ).normalized();
-
-        dir = unit_dir(pixel_origin, pos);
+        dir = (pos * (_focal_length / _sensor_lens_distance - 1.f) - 
+                (_focal_length / _sensor_lens_distance) * pixel_origin
+            ).normalized();
     }
 
 } 
--- src/camera.h
+++ src/camera.h
@@ -21,37 +21,48 @@ namespace Xrender
         camera(camera&&) = default;
         
         static camera from_sensor_lens_distance(
-            unsigned int sensor_pixel_width, unsigned int sensor_pixel_height,
-            float sensor_real_width, float sensor_real_height,
-            float diaphragm_radius, float focal_length, float sensor_lens_distance);
+            unsigned int image_pixel_width, 
+            unsigned int image_pixel_height,
+            float sensor_real_width, 
+            float sensor_real_height,
+            float diaphragm_radius, 
+            float focal_length, 
+            float sensor_lens_distance);
 
         static camera from_focus_distance(
-            unsigned int sensor_pixel_width, unsigned int sensor_pixel_height,
-            float sensor_real_with, float sensor_real_height,
-            float diaphragm_radius, float focal_length, float focus_distance);
+            unsigned int image_pixel_width, 
+            unsigned int image_pixel_height,
+            float sensor_real_with,
+            float sensor_real_height,
+            float diaphragm_radius,
+            float focal_length,
+            float focus_distance);
 
 
-        void sample_ray(unsigned int px, unsigned int py, vecf& pos, vecf& dir);
+        void sample_ray(unsigned int px, unsigned int py, vecf& pos, vecf& dir) const noexcept;
+
+        unsigned int get_image_width() const noexcept { return 2u * _image_pixel_half_width; }
+        unsigned int get_image_height() const noexcept { return 2u * _image_pixel_half_height; }
 
     private:
         camera(
-            unsigned int sensor_pixel_width,
-            unsigned int sensor_pixel_height,
+            unsigned int image_pixel_width,
+            unsigned int image_pixel_height,
             float sensor_real_width,
             float sensor_real_height,
             float diaphragm_radius,
             float focal_length,
             float sensor_lens_distance);
 
-        vecf _sample_lens_point();
+        vecf _sample_lens_point() const noexcept;
 
-        unsigned int _sensor_pixel_half_width;
-        unsigned int _sensor_pixel_half_height;
-        float _pixel_width;             // real with of a sensor pixel
-        float _pixel_height;            // real height of a sensor pixel
-        float _diaphragm_radius;
-        float _focal_length;
-        float _sensor_lens_distance;
+        const unsigned int _image_pixel_half_width;
+        const unsigned int _image_pixel_half_height;
+        const float _pixel_width;             // real with of a sensor pixel
+        const float _pixel_height;            // real height of a sensor pixel
+        const float _diaphragm_radius;
+        const float _focal_length;
+        const float _sensor_lens_distance;
     };
 
 } // namespace Xrender
