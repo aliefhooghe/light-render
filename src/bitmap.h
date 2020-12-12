296a013d760b9041e3149849c1847bd5f917904f
aliefhooghe <arthurliefh@orange.fr>
2020-12-13 00:18:20 +0100

    Fix rgb24 constructors

--- src/bitmap.h
+++ src/bitmap.h
@@ -5,38 +5,36 @@
 #include <stdint.h>
 #include <filesystem>
 
+#include "vec.h"
+
 namespace Xrender {
 
     struct rgb24  {
+
+        static rgb24 from_uint(uint8_t red, uint8_t green, uint8_t blue) noexcept
+        {
+            return {blue, green, red};
+        }
+
+        static rgb24 from_float(float red, float green, float blue)
+        {
+            return from_uint(
+                static_cast<uint8_t>(red * 255.f), 
+                static_cast<uint8_t>(green * 255.f),
+                static_cast<uint8_t>(blue * 255.f));
+        }
+
+        static rgb24 from_vecf(const vecf& color)
+        {
+            return from_float(color.x, color.y, color.z);
+        }
+
+
         uint8_t b;
         uint8_t g;
         uint8_t r;
     } __attribute__((__packed__));
 
-    inline rgb24 make_rgb24(uint8_t red, uint8_t green, uint8_t blue) noexcept
-    {
-        return rgb24{blue, green, red};
-    }
-
-    inline rgb24 make_rgb24(float red, float green, float blue) noexcept
-    {
-        return rgb24{
-            static_cast<uint8_t>(red * 255.f), 
-            static_cast<uint8_t>(green * 255.f),
-            static_cast<uint8_t>(red * 255.f)};
-    }
-
-    inline rgb24 make_rgb24(uint8_t monochrome) noexcept
-    {
-        return rgb24{monochrome, monochrome, monochrome};
-    };
-
-    inline rgb24 make_rgb24(float monochrome) noexcept
-    {
-        const auto val = static_cast<uint8_t>(monochrome * 255.f);
-        return rgb24{val, val, val};
-    };
-    
     void bitmap_write(
         const std::filesystem::path& path, const std::vector<rgb24>& data,
         unsigned int width, unsigned int height);
