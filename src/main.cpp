03f1aa359f73737c3c29056164ac72c820a79947
aliefhooghe <arthurliefh@orange.fr>
2020-12-13 16:44:05 +0100

    Add benchmark

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 
 
 #include <iostream>
-#include <chrono>
 
 #include "wavefront_obj.h"
 #include "bvh_tree.h"
@@ -9,29 +8,21 @@
 #include "outline_preview.h"
 #include "camera.h"
 
-  // const auto start = std::chrono::steady_clock::now();
-    // Xrender::bvh_tree tree{model};
-    // const auto end = std::chrono::steady_clock::now();
-    // const auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-    // const auto sec_duration = (float)duration / 1000.f;
-    // std::cout << "Took " << sec_duration << "seconds" << std::endl;
-
-
     
 int main()
 {   
-    auto model = Xrender::wavefront_obj_load("../../../render/Rendus/Nefertiti/nef.obj");
-    //auto model = Xrender::wavefront_obj_load("../../../render/Rendus/cube/untitled.obj");
+    //auto model = Xrender::wavefront_obj_load("../../../render/Rendus/Nefertiti/nef.obj");
+    auto model = Xrender::wavefront_obj_load("../untitled.obj");
     Xrender::bvh_tree tree{model};
     
     const auto width = 576;
     const auto height = 384;
 
     // Init camera
-    auto camera = Xrender::camera::from_sensor_lens_distance(width, height, 36E-3f, 24E-3f, 0, 25E-3f, 25E-3);
+    auto camera = Xrender::camera::from_focus_distance(width, height, 36E-3f, 24E-3f, 2E-3, 25E-3, 3.1f); //Xrender::camera::from_sensor_lens_distance(width, height, 36E-3f, 24E-3f, 0, 25E-3f, 25E-3);
 
     // Render preview
-    auto data = Xrender::render_outline_preview(tree, camera, 10);
+    auto data = Xrender::render_outline_preview(tree, camera, 1u);
 
 
     Xrender::bitmap_write("out.bmp", data, width, height);
